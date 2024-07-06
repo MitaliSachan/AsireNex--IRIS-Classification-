a74267dc549779d28b7c457542498b74fc2b7254
MitaliSachan <95533074+MitaliSachan@users.noreply.github.com>
2024-07-07 01:39:51 +0530

    Update iris_model.py

--- iris_model.py
+++ iris_model.py
@@ -1,9 +1,8 @@
 
 #Basic Libraries
+from sklearn.datasets import load_iris
 import numpy as np
 import pandas as pd
-from warnings import filterwarnings
-from collections import Counter
 
 
 # Data Pre-processing Libraries
@@ -11,19 +10,10 @@ from sklearn.preprocessing import StandardScaler,MinMaxScaler
 from sklearn.model_selection import train_test_split
 
 # Modelling Libraries
-from sklearn.linear_model import LogisticRegression,RidgeClassifier,SGDClassifier,PassiveAggressiveClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.svm import SVC,LinearSVC,NuSVC
-from sklearn.neighbors import KNeighborsClassifier,NearestCentroid
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier
-from sklearn.naive_bayes import GaussianNB,BernoulliNB
-from sklearn.ensemble import VotingClassifier
 
 # Evaluation & CV Libraries
 from sklearn.metrics import precision_score,accuracy_score
-from sklearn.model_selection import RandomizedSearchCV,GridSearchCV,RepeatedStratifiedKFold
-from sklearn.metrics import r2_score,mean_squared_error
 
 df=pd.read_csv('iris.csv')
 
