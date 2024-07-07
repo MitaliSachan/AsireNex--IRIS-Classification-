4af157d463c5801b4e899578513aab5a5bcb5c3b
MitaliSachan <95533074+MitaliSachan@users.noreply.github.com>
2024-07-07 10:35:43 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -31,7 +31,7 @@ def main():
 	# the font and background color, the padding and the text to be displayed
 	html_temp = """
 	<div style ="background-color:teal;padding:13px">
-	<h2 style ="color:black;text-align:center;">Streamlit Iris Flower Classifier</h2>
+	<h2 style ="color:white;text-align:center;">Streamlit Iris Flower Classifier</h2>
 	</div>
 	"""
   # this line allows us to display the front end aspects we have
